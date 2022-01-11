e7e23dd2f1c88823e90ae0cfc97778f84a156150
Aslam Jabri <jabri.aslam@gmail.com>
2022-01-11 22:40:57 +1100

    List

--- Introduction.py
+++ Introduction.py
@@ -47,3 +47,49 @@ print("Javascript" not in _list)
 num2 = [10,54,34,97,33,56,78,77]
 num2.sort()
 print(num2)
+
+
+#List Comprehension
+""" List Comprehension is a technique that uses a for loop and a condition to create a new list from an existing one. """
+# A result is always a new list, so its a good practice to assign list comprehension to a new variable.
+
+# Structure
+'''A list comprehension statement is always enclosed in square brackets,[].
+    [expression for loop if condition]      
+
+  1.  The expression is an operation used to create elements in the new list.
+
+  2.The for loop will iterate an existing list. The iterator will be used in the expression.
+
+  3. New elements will only be added to the new list when the if condition is fulfilled. This component is optional.
+'''
+ #if we use a simple iteration 
+numb = [1,2,3,4,5,6,7,8,9]
+numb_doub = []
+for i in numb:
+    numb_doub.append(i * 2)
+
+print(numb)
+print(numb_doub)
+
+
+#now lets convert to list comprehension
+
+number = [1,3,5,7,9]
+number_doub = [n * 2 for n in number]
+print(number)
+print(number_doub)
+
+
+#Adding a Condition
+numm = [10,20,30,40,50]
+numm_doub = [i * 2 for i in numm if i % 4 == 0]#only divisible by 4 will be multiplied
+
+print(numm)
+print(numm_doub)
+
+#using multiple lists
+n1 = [1,2,3,4,15]
+n2 = [7,8,19,5,6]
+sum_n=[(i,j) for i in n1 for j in n2 if i+j > 15]
+print(sum_n)
