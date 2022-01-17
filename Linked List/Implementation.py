be5d9ef05734f75e63acf1213ab04846bbee8bd4
Aslam Jabri <jabri.aslam@gmail.com>
2022-01-17 21:10:44 +1100

    Linked list Implementation

---
+++ Linked List/Implementation.py
@@ -0,0 +1,75 @@
+class Node:
+    def __init__(self,data) :
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+#PRINTING METHOD for the linked list
+#There you go! We initialize cur_node equal to the head of the linked list.
+# Then we use a while loop which keeps running and printing the data if cur_node is not equal to None.
+    def print_list(self):
+        cur_node = self.head
+        while cur_node:
+            print(cur_node.data)
+            cur_node = cur_node.next
+
+#EMPTY LIST       
+#Append method will insert an element at the end of the linked list
+#We define a new_node using our Node class on line 11. It consists of the data and the next field. 
+# We pass in data to the append method, and the data field in new_node has the entry of data that we passed to the append method.
+#For the Empty List    
+    def append(self,data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
+#In the above code, we check if the linked list is empty by checking the head of the linked list.
+#If the self.head is None on line 16, it implies that it’s an empty linked list and there’s nothing there.
+#The head pointer doesn’t point to anything at all, and therefore there is no node in the linked list. 
+#If there is no node in the linked list, we set the head pointer to the new_node that we created on line 17. 
+#In the next line, we simply return. 
+#The case of an empty linked list is relatively easy to handle.
+
+#For NON-EMPTY List:
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+#On line 27, we define last_node which is initially equal to the head. This implies we’re at the start of the linked list.
+#We have named the variable we defined on line 27 last_node​ because that’s what it will eventually point to. 
+#It will start at the beginning of the linked list and move through the linked list as long as the last_node.next doesn’t point to None. 
+#We keep moving from node to node on line 29 until we get to the last_node where last_node.next will point to None and will terminate the while loop on line 28.
+#After the while loop concludes, last_node points to the last node. On line 30, we input our new_node into the linked list by setting the next of last_node to new_node which has its own next pointing to None.
+
+#PREPEND METHOD
+    def prepend(self,data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+#We create a method called prepend. This also takes self and data since we need to tell it what to prepend to the linked list. We create a node based on the data passed into the method.
+# Next, on line 29, we point the next of the new_node to the current head of the linked list, and then we set the head of the linked list equal to new_node on line 50. 
+# We have now prepended E to llist in the code above which previously only contained A, B, and C. 
+# You can play around and verify the prepend method for yourself!
+
+#INSERT AFTER NODE
+    def insert_after_node(self,data,prev_node):
+        if not prev_node:
+            print("Previous node does not exist")
+            return
+        new_node = Node(data)
+        new_node.next = prev_node.next
+        prev_node.next = new_node
+
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.append("C")
+llist.append("D")
+llist.prepend("E")
+
+llist.print_list()
--- Linked List/Introduction.md
+++ Linked List/Introduction.md
@@ -2,3 +2,23 @@ There are three linked list
     Single linked list
     Double linked list
     Circular Linked list
+Every linked list consist of nodes. and every node has 2 components.
+    Data
+    Next
+The data component allows node in the linked list to store an element of data that can be of type string, haracter,number or any other type of object.
+The next component in every node is a pointer that points from one node to another.
+The start of the linked list is referred to as the head.head is a pointer that points to the beginning of the linked list, so if we want to traverse the linked list to obtain or access an element of the linked list we'll start from the head.
+The last component of a singly linked list is a notion of null. This null terminates the linked list . In python we call this None. The last node in a singly linked list points to a null object and that tells its the end of list.
+
+# Insertion And Deletion
+The insertion and deletion in arrays is O(n) operation. Lets assume in a given array if we want to insert an element in the first all the elements have to shift to left to make the room for the element to enter.Due to shifting of elements the time complexity is O(n). The same goes for the deletion as all the arrays have to be shifted to fill the gap in the array.
+
+Inserting a node at the head in the linked list given the head node is a constant-time operation need to change the orientation of few pointers. If given the exact pointer after which we have to insert another node ,it will be a constant-time operation . O(1)
+
+# Accessing Elements
+Accessing any element given an index in arrays is better than accessing nth elements in linked lists. It is a constant time O(1) operation to access elements in arrays. If given an array and an index, it can immediately give you the element at which the entry is stored. This is because arrays are contiguous.
+
+Accessing an nth element in a linked list is an O(n) operation given that you have access to the head node of the linked list. If we want to access an element, we need to start from the head pointer and traverse the entire linked list before we can get to it.
+
+# Contiguos Memory
+Arrays are contiguous in memory which allows the access time to be constant, whereas, in linked lists, you do not have the luxury of contiguous memory.
